006ec8a93cf599b3a12be95c3d4e14188162dec2
Peter Danenberg <pcd@roxygen.org>
2013-04-07 14:35:19 -0700

    Regular mplayer now

--- online-navigation.scm
+++ online-navigation.scm
@@ -198,11 +198,11 @@
           (debug (agent-score agent))
           (when animation-file
             (finalize! animation-file)
-            (run (sudo -E vt mplayer -really-quiet -vo fbdev2 -loop 0 ,animation-file))
+            ;; (run (sudo -E vt mplayer -really-quiet -vo fbdev2 -loop 0 ,animation-file))
             ;; sudo mplayer -really-quiet -vo fbdev2 -vf scale=1440:900 online-dfs-random-statistics.avi
             ;; (run (sudo mplayer -vo fbdev2 -loop 0 ,animation-file))
             ;; (run (vt mplayer -really-quiet -vo fbdev2 -loop 0 ,animation-file))
-            ;; (run (mplayer -really-quiet -loop 0 ,animation-file))
+            (run (mplayer -really-quiet -loop 0 ,animation-file))
             ))))))
 
 ;; Simulation:1 ends here
