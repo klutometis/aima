23a8f9ea5047b074ce9fbd2b14d17beaafbac641
Peter Danenberg <pcd@roxygen.org>
2013-04-07 17:58:17 -0700

    Inchoate LRTA*

--- learning-real-time-a-star.scm
+++ learning-real-time-a-star.scm
@@ -4,12 +4,23 @@
 
 (include "online-navigation.scm")
 
+;;; Let's keep track of the goal and visualize a straight-line
+;;; heuristic thither in light grey (with the updated distance) for
+;;; those states for which we have information.
 (define (make-agent-lrta* start next-frame)
   (make-agent
    start
    0
-   (lambda (state goal? score)
-     (car state))))
+   (let ((result (make-hash-table))
+         (cost-estimate (make-hash-table))
+         (previous-state #f)
+         (previous-action #f))
+     (lambda (state goal? score)
+       (if goal?
+           zero-motion
+           (begin
+             (unless (hash-table-exists? cost-estimate state)
+               (hash-table-set! cost-estimate))))))))
 
 (simulate-navigation make-agent-lrta*)
 
