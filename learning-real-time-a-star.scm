72dfc3c66627b8f4d59a857e0dabd7c89b13f421
Peter Danenberg <pcd@roxygen.org>
2013-04-08 17:50:15 -0700

    Graph it with heuristic!

--- learning-real-time-a-star.scm
+++ learning-real-time-a-star.scm
@@ -5,6 +5,18 @@
 (include "online-navigation.scm")
 (use heap)
 
+(define (write-dot-preamble width height title)
+ (display "digraph G {")
+ (display "node [shape=point];")
+ (let ((width-in-inches (/ width 96))
+       (height-in-inches (/ height 96)))
+   (format #t "graph [fontsize=48, label=\"~a\", ratio=fill, viewport=\"~a,~a\", size=\"~a,~a!\", labelloc=t];"
+           title
+           (* width-in-inches 72)
+           (* height-in-inches 72)
+           width-in-inches
+           height-in-inches)))
+
 ;;; Let's keep track of the goal and visualize a straight-line
 ;;; heuristic thither in light grey (with the updated distance) for
 ;;; those states for which we have information.
@@ -14,8 +26,11 @@
    0
    (let ((result (make-hash-table))
          (cost-estimate (make-hash-table))
+         (coordinates (make-hash-table))
          (previous-state #f)
-         (previous-action #f))
+         (previous-action #f)
+         (time 0)
+         (labels (make-hash-table)))
      (define (lrta*-cost previous-state previous-action state)
        (if state
            (+ (point-distance origin previous-action)
@@ -68,9 +83,109 @@
 
      (define (reset-state!)
        (set! previous-state #f)
-       (set! previous-action #f))
+       (set! previous-action #f)
+       (inc! time))
+
+     (define (update-labels! state)
+       (unless (hash-table-exists? labels state)
+         (hash-table-set!
+          labels
+          state
+          (gensym))))
+
+     (define (update-coordinates! state)
+       (let* ((previous-coordinate
+               (hash-table-ref/default
+                coordinates
+                previous-state
+                (make-coordinate origin time)))
+              (previous-point
+               (coordinate-point previous-coordinate)))
+         (if (hash-table-exists? coordinates state)
+             (let* ((coordinate (hash-table-ref coordinates state))
+                    (point (coordinate-point coordinate)))
+               (when (< (coordinate-time coordinate) time)
+                 (let ((delta-x
+                        (- (point-x point)
+                           (+ (point-x previous-point)
+                              (point-x previous-action))))
+                       (delta-y
+                        (- (point-y point)
+                           (+ (point-y previous-point)
+                              (point-y previous-action)))))
+                   (hash-table-walk coordinates
+                     (lambda (state old-coordinate)
+                       (when (< (coordinate-time old-coordinate) time)
+                         (coordinate-time-set! old-coordinate time)
+                         (let ((old-point (coordinate-point old-coordinate)))
+                           (coordinate-point-set!
+                            old-coordinate
+                            (make-point (- (point-x old-point) delta-x)
+                                        (- (point-y old-point) delta-y))))))))))
+             (hash-table-set!
+              coordinates
+              state
+              (make-coordinate
+               (make-point (+ (point-x previous-point)
+                              (point-x previous-action))
+                           (+ (point-y previous-point)
+                              (point-y previous-action)))
+               time)))))
+
+     (define (write-agent-as-dot state)
+       (let ((displayed (make-hash-table))
+             (linear-scale (* 5 72)))
+
+         (define (node-display state label)
+           (unless (hash-table-exists? displayed state)
+             (hash-table-set! displayed state #t)
+             (let ((coordinate
+                    (hash-table-ref/default coordinates
+                                            state
+                                            (make-coordinate origin time))))
+               (format #t "~a [pos=\"~a,~a\", xlabel=\"~,2f\"];"
+                       label
+                       (* (point-x (coordinate-point coordinate)) linear-scale)
+                       (* (point-y (coordinate-point coordinate)) linear-scale)
+                       (hash-table-ref cost-estimate state)))))
+
+         ;; (write-dot-preamble 800 450 "Random walk with error correction")
+         (write-dot-preamble 1600 900 "LRTA*")
+
+         (hash-table-walk result
+           (lambda (whence whither->action)
+             (hash-table-walk whither->action
+               (lambda (action whither)
+                 (let ((whence-label
+                        (hash-table-ref labels whence))
+                       (whither-label
+                        (hash-table-ref labels whither)))
+                   (node-display whence whence-label)
+                   (node-display whither whither-label)
+                   (format #t "~a -> ~a [color=~a, penwidth=~a];"
+                           whence-label
+                           whither-label
+                           (if (equal? whence previous-state)
+                               "orange"
+                               "blue")
+                           (if (equal? whence previous-state)
+                               2
+                               1)))))))
+         (display "}")))
+
+     (define (write-agent-as-png png state)
+       (let ((dot (create-temporary-file ".dot")))
+         (with-output-to-file dot
+           (lambda ()
+             (write-agent-as-dot state)))
+         (run (neato -n1 -Tpng -o ,png < ,dot))))
 
      (lambda (state goal? score)
+       (update-labels! state)
+       (when previous-state
+         (update-coordinates! state))
+       (unless (zero? (hash-table-size result))
+         (write-agent-as-png (next-frame) state))
        (if goal?
            (begin
              (reset-state!)
@@ -99,7 +214,8 @@
 
 (simulate-navigation make-agent-lrta*
                      ;; make-agent-random-walk
-                     n-nodes: 100
-                     n-steps: 10000)
+                     n-nodes: 10
+                     n-steps: 100
+                     animation-file: "learning-real-time-a-star.avi")
 
 ;; 4\.13:1 ends here
