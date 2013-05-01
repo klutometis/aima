0bc1625c1b5b9609ff34490b78e2ac802ed698f6
Peter Danenberg <pcd@roxygen.org>
2013-04-30 19:43:44 -0700

    Record the character->point mapping.

--- scrabble.scm
+++ scrabble.scm
@@ -430,6 +430,36 @@
       (vector-set! v r (vector-ref v (- n 1)))
       (vector-set! v (- n 1) t))))
 
+(define character->points
+  (alist->hash-table
+   `((,sentinel . 0)
+     (#\E . 1)
+     (#\A . 1)
+     (#\I . 1)
+     (#\O . 1)
+     (#\N . 1)
+     (#\R . 1)
+     (#\T . 1)
+     (#\L . 1)
+     (#\S . 1)
+     (#\U . 1)
+     (#\D . 2)
+     (#\G . 2)
+     (#\B . 3)
+     (#\C . 3)
+     (#\M . 3)
+     (#\P . 3)
+     (#\F . 4)
+     (#\H . 4)
+     (#\V . 4)
+     (#\W . 4)
+     (#\Y . 4)
+     (#\K . 5)
+     (#\J . 8)
+     (#\X . 8)
+     (#\Q . 10)
+     (#\Z . 10))))
+
 ;;; Fuck; forgot about points per tile. We can have that in a table
 ;;; elsewhere, though. Display it somehow?
 (define (make-scrabble-tiles)
