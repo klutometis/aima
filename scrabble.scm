b862c64dc85ec2becdf078394510e83413ef61af
Peter Danenberg <pcd@roxygen.org>
2013-05-01 14:48:53 -0700

    Parameterize some constants.

--- scrabble.scm
+++ scrabble.scm
@@ -15,21 +15,27 @@
 (define-record-and-printer sentinel)
 (define sentinel (make-sentinel))
 
+(define sentinel-index (make-parameter 26))
+(define terminal-index (make-parameter 27))
+
 (define (character->index character)
   (if (sentinel? character)
-      26
+      (sentinel-index)
       (- (char->integer character) 65)))
 
 (define (index->character index)
-  (if (= index 26)
+  (if (= index (sentinel-index))
       sentinel
       (integer->char (+ index 65))))
 
-;;; Can get this down to bits?
-(define (make-dag) (make-vector 28 #f))
-(define (dag-terminal? dag) (vector-ref dag 27))
+(define dag-length (make-parameter (+ 26 ; Alphabet
+                                      1 ; Sentinel
+                                      1 ; Terminal?
+                                      )))
+(define (make-dag) (make-vector (dag-length) #f))
+(define (dag-terminal? dag) (vector-ref dag (terminal-index)))
 (define (dag-terminal?-set! dag terminal?)
-  (vector-set! dag 27 terminal?))
+  (vector-set! dag (terminal-index) terminal?))
 (define (dag-ref dag character)
   (vector-ref dag (character->index character)))
 (define (dag-set! dag character value)
