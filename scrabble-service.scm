f153c72988c9b370179e97c6394bc33be29efca6
Peter Danenberg <pcd@roxygen.org>
2013-06-12 03:30:12 -0700

    Some hybrid, non-functional state

--- scrabble-service.scm
+++ scrabble-service.scm
@@ -27,8 +27,17 @@
   id
   pubsub-url)
 
-(define-record-and-printer user
+(define-record-and-printer player
   id)
+
+;; (thread-start!
+;;  (lambda ()
+;;    ;; (let ((socket (make-socket 'pub)))
+;;    ;;   (bind-socket socket "tcp://*:5555")
+;;    ;;   (let iter ()
+;;    ;;     (debug (send-message socket (->string (hash-table->alist games))))
+;;    ;;     (iter)))
+;;    2))
 
 (call-with-dynamic-fastcgi-query
  (lambda (query)
@@ -44,16 +53,31 @@
      ("/join"
       (let ((game-id (query-client-any query 'game-id))
             (player-id (generate-id)))
-        (let ((game (hash-table-ref/default games game-id #f)))
+        (let ((game (hash-table-ref/default games game-id #f))
+              (player (make-player player-id)))
           (if game
-              (begin
+              (let* ((player-id (generate-id))
+                     (player (make-player player-id)))
+                (hash-table-set! players player-id player)
                 (display-content-type-&c. 'json)
                 (write-json `((player-id . ,player-id)
                               (pubsub-url . ,(game-pubsub-url game)))))
               (display-status-&c. status-not-found)))))
-     ("/list"
+     ("/list-games"
       (display-content-type-&c. 'json)
       (write-json `((game-ids . ,(list->vector (hash-table-keys games))))))
-     (_ (pp "yes")))))
+     ("/list-players"
+      (display-content-type-&c. 'json)
+      (write-json `((players-ids . ,(list->vector (hash-table-keys players))))))
+     ("/play"
+      (let ((game-id (query-client-any query 'game-id))
+            (player-id (query-client-any query 'player-id))
+            (move (query-client-any query 'move)))
+        (let ((game (hash-table-ref/default games game-id #f))
+              (player (hash-table-ref/default players player-id #f)))
+          (if (and game player)
+              (display-content-type-&c. 'json)
+              (display-status-&c. status-not-found)))))
+     (_ (display-status-&c. status-bad-request)))))
 
 ;; 5\.5:10 ends here
