e02a494c29aa5d8d0352d1dbce85784155c60343
Peter Danenberg <pcd@roxygen.org>
2013-07-29 04:59:50 -0700

    Do the no-outliers plot.

--- random-coloring.R
+++ random-coloring.R
@@ -1,6 +1,7 @@
 library(debug)
 library(ggplot2)
 data <- read.csv('random-coloring-data.csv', header=T, sep=',')
+
 png("random-coloring.png", width=1600, height=900)
 ggplot(data=data, aes(x=n, y=time, col=index)) +
   geom_point() +
@@ -11,4 +12,16 @@ ggplot(data=data, aes(x=n, y=time, col=index)) +
                          "Min-conflicts")) +
   scale_x_continuous(name="n") +
   scale_y_continuous(name="Time (s)")
+dev.off()
+
+png("random-coloring-no-outliers.png", width=1600, height=900)
+ggplot(data=data, aes(x=n, y=time, col=index)) +
+  geom_point() +
+  geom_smooth() +
+  scale_color_discrete(name="Method",
+                       labels=c("Backtracking",
+                         "Foward checking",
+                         "Min-conflicts")) +
+  scale_x_continuous(name="n") +
+  scale_y_continuous(name="Time (s)", limits=c(0, 1.5))
 dev.off()
